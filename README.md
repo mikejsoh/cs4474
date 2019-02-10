4441b704b7dc8f8372c89e4fd6d2f88f37cb98cc
kbhaskar7 <karthik07070@gmail.com>
2019-02-09 21:27:40 -0500

    Basic tutorial app to get started

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cs4474
+    In order to run:
+1. Install node and npm on machine 
+2. "npm install" from root directory that has package.json
+3. "npm start" to start server that automatically refreshes on changes 
---
+++ app.js
@@ -0,0 +1,42 @@
+//import needed libraries 
+const koa = require('koa');
+const json = require('koa-json');
+const koaRouter = require('koa-router');
+const koaEjs = require('koa-ejs');
+const path = require('path');
+
+//initialize app as instance of koa 
+const app = new koa();
+//initialize router 
+const router = new koaRouter();
+
+//Pretty print JSON middleware
+app.use(json());
+
+//Ejs templating options, injects views into layout.html
+koaEjs(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'layout',
+    viewExt: 'html',
+    cache: false,
+    debug: false
+});
+
+//Route mapping 
+router.get('/', index);
+
+//Router functions
+async function index(ctx){
+    await ctx.render('index', {
+        title: 'All my Tasks:'
+    });
+};
+
+//for testing purposes
+router.get('/test', ctx => (ctx.body = 'Hello World'));
+
+//Router Middle ware, lets app use routes and allows get post method on routes
+app.use(router.routes()).use(router.allowedMethods());
+
+//make the app listen to port 3000 and log message 
+app.listen(3000, () => console.log('Server Started ...'));
